2d4a793353fa03bad56105847273ef0cbaec404e
Abhishek <abhishek.gupta@hindustantimes.com>
2017-06-15 16:43:34 +0530

    viral projects

---
+++ app.js
@@ -0,0 +1,80 @@
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('error-handler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+
+var http = require('http');
+var request = require('request');
+var cheerio = require('cheerio');
+var path = require('path');
+var engines = require('consolidate');
+
+var app = module.exports = express();
+
+
+/**
+ * Configuration
+ */
+
+// all environments
+var k ="3d8cbec88571463382ad6c808ab7f599";
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
+
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+
+// development only
+if (env === 'development') {
+  app.use(express.errorHandler());
+}
+
+// production only
+if (env === 'production') {
+  // TODO
+}
+
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
